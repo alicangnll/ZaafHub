318d750a1f74daba85080540e10c7c2343f92008
Ali Can Gönüllü <alicangonullu@yahoo.com>
2022-10-26 14:31:49 +0300

    Index Editted
    Index Editted

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
   </head>
   <body class="text-center container">
-    <div style="padding:15px;" class="mt-4 bg-secondary">
+    <div style="padding:15px;" class="mt-4">
       <p align="right" style="font-size:200%" class="text-center">Ali Can Gonullu | Vulnerable Web Application</p>
     </div>
     <hr></hr>
