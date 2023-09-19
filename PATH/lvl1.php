fddd225a228f582be3b40c3356654b8064cca299
Ali Can Gönüllü <alicangonullu@yahoo.com>
2023-09-19 19:59:12 +0300

    Path Travelsal Updated
    Path Travelsal Updated

--- PATH/lvl1.php
+++ PATH/lvl1.php
@@ -15,6 +15,7 @@
         <input type="submit" />
     </form>
   </div>
+  <!-- <script src="dsc/main.js"></script> -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
   <script>
   function deleteAllCookies() {
