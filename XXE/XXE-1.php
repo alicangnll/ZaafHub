e548009ae6624672a52b27a2f3f1f09cdaf09194
Ali Can Gönüllü <alicangonullu@yahoo.com>
2023-09-19 19:53:42 +0300

    XXE Bug Update
    XXE Bug Update

--- XXE/XXE-1.php
+++ XXE/XXE-1.php
@@ -14,6 +14,15 @@
       <input type="text" name="xml" placeholder="XML File" /><br>
       <input type="submit" />
     </form>
+    <div class="mt-4 container">
+      <b>XML List</b>
+      <ul>
+        <li>lin.xml</li>
+        <li>users.xml</li>
+        <li>win.xml</li>
+        <li>passwd.xml</li>
+      </ul>
+    </div>
   </div>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
   <script>
